84007c0c6f7cb9fefafacba3b5289dacc84d9ff9
giuliabriao <briaogiulia@gmail.com>
2021-05-12 16:58:43 -0300

    CRUD de usuarios feita, seed inclusive

---
+++ src/controllers/UserController.js
@@ -0,0 +1,88 @@
+const knex = require('../database');
+const bcrypt = require('bcrypt');
+
+module.exports = {
+    async index(req, res) {
+        const results = await knex('users')
+        // .where("deleted_at", null)
+        delete results.password
+        return res.json(results)
+        //temos que esconder o password do GET
+    },
+    async create(req, res, next) {
+
+        try {
+            const {
+                firstName,
+                lastName,
+                username,
+                avatar,
+                email,
+                birth,
+                password
+            } = req.body
+
+            const passwordHash = await bcrypt.hash(password, 8)
+
+            await knex('users').insert({
+                firstName,
+                lastName,
+                username,
+                avatar,
+                email,
+                birth,
+                password: passwordHash
+            });
+            return res.status(201).send()
+        } catch (error) {
+            next(error)
+        }
+    },
+    async update(req, res, next) {
+        try {
+            const {
+                firstName,
+                lastName,
+                username,
+                avatar,
+                email,
+                birth,
+                password
+            } = req.body
+
+            const passwordHash = await bcrypt.hash(password, 8)
+
+            const { id } = req.params
+
+            await knex('users')
+                .update({
+                    firstName,
+                    lastName,
+                    username,
+                    avatar,
+                    email,
+                    birth,
+                    password: passwordHash
+                })
+                .where({ id })
+
+            return res.send()
+        } catch (error) {
+            next(error)
+        }
+    },
+    async delete(req, res, next) {
+        try {
+            const { id } = req.params
+
+            await knex('users')
+                .where({ id })
+                // .update("deleted_at", new Date())
+             .del()
+
+            return res.send()
+        } catch (error) {
+            next(error)
+        }
+    }
+}
--- src/database/routes.js
+++ src/database/routes.js
@@ -2,6 +2,12 @@ const express = require('express');
 const routes = express.Router();
 
 const UserController = require('../controllers/UserController');
-const ProjectController = require('../controllers/ProjectController');
+// const ProjectController = require('../controllers/ProjectController');
+
+routes
+    .get('/users', UserController.index)
+    .post('/user', UserController.create)
+    .put('/user/:id', UserController.update)
+    .delete('/user/:id', UserController.delete)
 
 module.exports = routes
