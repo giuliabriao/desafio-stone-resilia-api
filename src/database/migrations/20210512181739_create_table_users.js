38a9dd3e1eeb8d4331f95f1d22a78dc1965445f5
giuliabriao <briaogiulia@gmail.com>
2021-05-12 16:09:27 -0300

    migration de criacao da tabela usuarios feita

--- src/database/migrations/20210512181739_create_table_users.js
+++ src/database/migrations/20210512181739_create_table_users.js
@@ -1,8 +1,20 @@
+// const { onUpdateTrigger } = require('../../../knexfile')
 
-exports.up = function(knex) {
-  
-};
+exports.up = async knex => knex.schema.createTable('users', table => {
+    table.increments('id')
+    table.text('firstName').notNullable()
+    table.text('lastName').notNullable()
+    table.text('username').unique().notNullable()
+    table.string('avatar')
+    table.text('email').unique().notNullable()
+    table.date('birth').unique().notNullable()
+    table.string('password').notNullable()
 
-exports.down = function(knex) {
-  
-};
+    
+    table.timestamp('created_at').defaultTo(knex.fn.now())
+    table.timestamp('updated_at').defaultTo(knex.fn.now())
+})
+// .then( () => knex.raw(onUpdateTrigger('users')))
+
+
+exports.down = async knex => knex.schema.dropTable('users')
