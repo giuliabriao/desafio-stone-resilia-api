16c8ed1096b2c8adc12ac54f2808853c06dc0800
BrunoSampaioDev <sampaionascimento100@gmail.com>
2021-05-18 03:43:56 -0300

    feat: solução temporaria para remover o password da listagem de users  ✨

--- src/services/users/ListUserService.js
+++ src/services/users/ListUserService.js
@@ -5,6 +5,10 @@ module.exports = {
 
   async index() {
     const users = await knex('users')
+      .select("firstName","lastName","username","avatar","email","birth")
+    
+    delete users
+
     return users
   },
 }
