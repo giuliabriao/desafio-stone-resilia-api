1c77b6e4e2d6c2ba6747fcc6fe847b9c2c4a89d7
BrunoSampaioDev <sampaionascimento100@gmail.com>
2021-05-19 16:55:59 -0300

    merge: Giu  ✨

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      database: "postgres",
+      database: "stone",
       user: "postgres",
       password: "2198347465",
     },
