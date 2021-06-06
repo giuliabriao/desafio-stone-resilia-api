3459bfbe0ed3b3105d8bbb37e2a337025d649db3
BrunoSampaioDev <sampaionascimento100@gmail.com>
2021-06-05 21:41:29 -0300

    fix: remove console.log

--- src/services/users/UpdateAvatarService.js
+++ src/services/users/UpdateAvatarService.js
@@ -2,7 +2,6 @@ const knex = require("../../database");
 
 module.exports = {
   async update(url, id) {
-    console.log(url);
     await knex("users").update({ avatar: url }).where({ id });
   },
 };
